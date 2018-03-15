784a500d9aa324f1ba6b15cf833047ad0e9dba9b
David Weitzman <dweitzman@pinterest.com>
2018-03-15 14:07:21 -0700

    Do not raise an error in bazel_bootstrap if go_repos.bzl does not exists yet
    Reviewers: laurabiester
    
    Reviewed By: laurabiester
    
    Differential Revision: https://phabricator.pinadmin.com/D257842

--- bazel_bootstrap.sh
+++ bazel_bootstrap.sh
@@ -7,6 +7,10 @@
 # You'll need to run this before building for the first time,
 # or if you've added go files or changed their imports.
 
+if [ ! -e "go_repos.bzl" ] ; then
+  echo "def register_go_repos():" > go_repos.bzl
+  echo "  pass" >> go_repos.bzl
+fi
 bazel build :generate_go_repos
 cp bazel-genfiles/go_repos.bzl go_repos.bzl
 chmod +w go_repos.bzl
