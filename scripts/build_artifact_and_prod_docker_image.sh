6d1c57e69a82dd010db4fd3571136c26fada7aaf
David Weitzman <dweitzman@pinterest.com>
2018-04-13 18:16:14 -0700

    No need for an interactive tty
    Summary: Signed-off-by: David Weitzman <dweitzman@pinterest.com>
    
    Reviewers: laurabiester
    
    Differential Revision: https://phabricator.pinadmin.com/D267688
    
    Signed-off-by: David Weitzman <dweitzman@pinterest.com>

--- scripts/build_artifact_and_prod_docker_image.sh
+++ scripts/build_artifact_and_prod_docker_image.sh
@@ -27,7 +27,7 @@ mkdir -p $OUTPUT_LOCAL_DIR
 
 echo "Building :full_dist as $FULL_OUTPUT_FILE using a dockerized version of bazel..."
 
-docker run -it -v `pwd`:/vt/src -v $OUTPUT_LOCAL_DIR:/vt/build_artifacts -w /vt/src --entrypoint /bin/bash 998131032990.dkr.ecr.us-east-1.amazonaws.com/bazel:latest -c "./bazel_bootstrap.sh && bazel build --symlink_prefix=/ :full_dist && cp -f \`bazel info bazel-bin\`/full_dist.tar.gz /vt/build_artifacts/$OUTPUT_TARBALL_NAME"
+docker run -t -v `pwd`:/vt/src -v $OUTPUT_LOCAL_DIR:/vt/build_artifacts -w /vt/src --entrypoint /bin/bash 998131032990.dkr.ecr.us-east-1.amazonaws.com/bazel:latest -c "./bazel_bootstrap.sh && bazel build --symlink_prefix=/ :full_dist && cp -f \`bazel info bazel-bin\`/full_dist.tar.gz /vt/build_artifacts/$OUTPUT_TARBALL_NAME"
 
 
 echo Finished building :full_dist as $FULL_OUTPUT_FILE
