98f3151269028a36ddc40f1f32a4eadf6ffb19dc
David Weitzman <dweitzman@pinterest.com>
2018-08-16 13:43:09 -0700

    Add a vindex that's like "hash", but allows an additive offset.
    Summary:
    This will be useful specifically for the "advertisers" table which only has
    local IDs today during row inserts without any GID yet. Other tables will
    have advertiser GIDs as they are created.
    
    If we fully migrate to vitess sequences we can actually arrange to set both the
    local and GID during the initial insert, and eventually we'll replace all the
    local IDs with GIDs. It's possible we won't need this hash_offset. If not, we
    can delete it later.
    
    Reviewers: vamsi, sreshtav, chengchenghu, epruitt
    
    Reviewed By: chengchenghu
    
    Differential Revision: https://phabricator.pinadmin.com/D308766

---
+++ go/vt/vtgate/vindexes/hash_offset.go
@@ -0,0 +1,105 @@
+package vindexes
+
+// NOTE(dweitzman): This is a modified version of hash.go
+
+import (
+	"fmt"
+	"strconv"
+
+	"vitess.io/vitess/go/sqltypes"
+	"vitess.io/vitess/go/vt/key"
+)
+
+var (
+	_ Vindex     = (*HashOffset)(nil)
+	_ Reversible = (*HashOffset)(nil)
+)
+
+// HashOffset defines vindex that adds or subtracts a constant to column values
+// before delegating to the Hash vindex. At Pinterest, this will be useful for
+// the "advertisers" table in the short term because of advertiser local IDs vs
+// GIDs. In time we'll want to replace all the local IDs with their GIDs and
+// deprecate the concept of GIDs, but for the short term it's useful to have
+// the flexibility to use either a local or a gid column when defining a primary
+// vindex, since the "advertisers" table doesn't yet use sequences and doesn't
+// yet have the ability to set a gid during row creation (just local IDs).
+type HashOffset struct {
+	hash   Hash
+	offset uint64
+}
+
+// NewHashOffset creates a new HashOffset.
+func NewHashOffset(name string, m map[string]string) (Vindex, error) {
+	offsetStr, ok := m["offset"]
+	if !ok || offsetStr == "" {
+		return nil, fmt.Errorf("hash_offset.NewHashOffset: 'offset' param missing")
+	}
+
+	offset, err := strconv.ParseInt(offsetStr, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf(`hash_offset.NewHashOffset: "%v" is not a valid offset`, offsetStr)
+	}
+
+	return &HashOffset{hash: Hash{name: name}, offset: uint64(offset)}, nil
+}
+
+// String returns the name of the vindex.
+func (vind *HashOffset) String() string {
+	return vind.hash.String()
+}
+
+// Cost returns the cost of this index as 1.
+func (vind *HashOffset) Cost() int {
+	return vind.hash.Cost()
+}
+
+// IsUnique returns true since the Vindex is unique.
+func (vind *HashOffset) IsUnique() bool {
+	return true
+}
+
+// IsFunctional returns true since the Vindex is functional.
+func (vind *HashOffset) IsFunctional() bool {
+	return true
+}
+
+func applyOffset(offset uint64, ids []sqltypes.Value) []sqltypes.Value {
+	translated := make([]sqltypes.Value, 0, len(ids))
+	for _, id := range ids {
+		num, err := sqltypes.ToUint64(id)
+		if err != nil {
+			// Not a unint64, so leave it alone.
+			// The underlying Hash implementation will ignore nulls or report bad types.
+			translated = append(translated, id)
+		} else {
+			// Apply offset to numbers.
+			adjustedNum := num + offset
+			translated = append(translated, sqltypes.NewUint64(adjustedNum))
+		}
+	}
+	return translated
+}
+
+// Map can map ids to key.Destination objects.
+func (vind *HashOffset) Map(cursor VCursor, ids []sqltypes.Value) ([]key.Destination, error) {
+	return vind.hash.Map(cursor, applyOffset(vind.offset, ids))
+}
+
+// Verify returns true if ids maps to ksids.
+func (vind *HashOffset) Verify(cursor VCursor, ids []sqltypes.Value, ksids [][]byte) ([]bool, error) {
+	return vind.hash.Verify(cursor, applyOffset(vind.offset, ids), ksids)
+}
+
+// ReverseMap returns the ids from ksids.
+func (vind *HashOffset) ReverseMap(cursor VCursor, ksids [][]byte) ([]sqltypes.Value, error) {
+	vals, err := vind.hash.ReverseMap(cursor, ksids)
+	if err != nil {
+		return nil, err
+	}
+
+	return applyOffset(-vind.offset, vals), nil
+}
+
+func init() {
+	Register("hash_offset", NewHashOffset)
+}
---
+++ go/vt/vtgate/vindexes/hash_offset_test.go
@@ -0,0 +1,196 @@
+package vindexes
+
+// NOTE(dweitzman): This is a modified version of hash_test.go
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"vitess.io/vitess/go/sqltypes"
+	"vitess.io/vitess/go/vt/key"
+)
+
+func createHashOffset(offset string) Vindex {
+	hv, err := CreateVindex("hash_offset", "nn", map[string]string{"offset": offset})
+	if err != nil {
+		panic(err)
+	}
+	return hv
+}
+
+func TestHashOffsetCreation(t *testing.T) {
+	tcases := []struct {
+		offset, wantErr string
+	}{
+		{
+			offset:  "",
+			wantErr: "hash_offset.NewHashOffset: 'offset' param missing",
+		},
+		{
+			offset:  "weird",
+			wantErr: `hash_offset.NewHashOffset: "weird" is not a valid offset`,
+		},
+		{
+			offset:  "1.2",
+			wantErr: `hash_offset.NewHashOffset: "1.2" is not a valid offset`,
+		},
+	}
+
+	for _, tcase := range tcases {
+		params := map[string]string{"offset": tcase.offset}
+		_, err := CreateVindex("hash_offset", "nn", params)
+		if err == nil || err.Error() != tcase.wantErr {
+			t.Errorf(`CreateVindex("hash_offset", "nn", %v) had err %v. Want: %v`, params, err, tcase.wantErr)
+		}
+	}
+}
+
+func TestHashOffsetFunc(t *testing.T) {
+	tcases := []struct {
+		offset   uint64
+		in, want []sqltypes.Value
+	}{{
+		offset: 1,
+		in:     []sqltypes.Value{sqltypes.NewUint64(1), sqltypes.NewUint64(100)},
+		want:   []sqltypes.Value{sqltypes.NewUint64(2), sqltypes.NewUint64(101)},
+	}, {
+		offset: 1,
+		in:     []sqltypes.Value{sqltypes.NewUint64(1), sqltypes.NULL, sqltypes.NewUint64(100)},
+		want:   []sqltypes.Value{sqltypes.NewUint64(2), sqltypes.NULL, sqltypes.NewUint64(101)},
+	}, {
+		offset: ^uint64(0),
+		in:     []sqltypes.Value{sqltypes.NewUint64(1), sqltypes.NULL, sqltypes.NewUint64(100)},
+		want:   []sqltypes.Value{sqltypes.NewUint64(0), sqltypes.NULL, sqltypes.NewUint64(99)},
+	}, {
+		offset: 2,
+		in:     []sqltypes.Value{sqltypes.NewUint64(1), sqltypes.NewUint64(100)},
+		want:   []sqltypes.Value{sqltypes.NewUint64(3), sqltypes.NewUint64(102)},
+	}}
+
+	for _, tcase := range tcases {
+		got := applyOffset(tcase.offset, tcase.in)
+		if !reflect.DeepEqual(tcase.want, got) {
+			t.Errorf("applyOffset(%v, %v) = %v, want %v", tcase.offset, tcase.in, got, tcase.want)
+		}
+	}
+}
+
+func TestHashOffsetCost(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	if hashOffset.Cost() != 1 {
+		t.Errorf("Cost(): %d, want 1", hashOffset.Cost())
+	}
+}
+
+func TestHashOffsetString(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	if strings.Compare("nn", hashOffset.String()) != 0 {
+		t.Errorf("String(): %s, want hashOffset", hashOffset.String())
+	}
+}
+
+func TestHashOffsetMap(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	got, err := hashOffset.Map(nil, []sqltypes.Value{
+		sqltypes.NewInt64(0),
+		sqltypes.NewInt64(1),
+		sqltypes.NewInt64(2),
+		sqltypes.NULL,
+		sqltypes.NewInt64(3),
+		sqltypes.NewInt64(4),
+		sqltypes.NewInt64(5),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	want := []key.Destination{
+		key.DestinationKeyspaceID([]byte("\x16k@\xb4J\xbaK\xd6")),
+		key.DestinationKeyspaceID([]byte("\x06\xe7\xea\"Βp\x8f")),
+		key.DestinationKeyspaceID([]byte("N\xb1\x90ɢ\xfa\x16\x9c")),
+		key.DestinationNone{},
+		key.DestinationKeyspaceID([]byte("\xd2\xfd\x88g\xd5\r-\xfe")),
+		key.DestinationKeyspaceID([]byte("p\xbb\x02<\x81\f\xa8z")),
+		key.DestinationKeyspaceID([]byte("\xf0\x98H\n\xc4ľq")),
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Map(): %#v, want %+v", got, want)
+	}
+}
+
+func TestHashOffsetMapNegative(t *testing.T) {
+	hashOffset := createHashOffset("-1")
+
+	got, err := hashOffset.Map(nil, []sqltypes.Value{
+		sqltypes.NewInt64(2),
+		sqltypes.NewInt64(3),
+		sqltypes.NewInt64(4),
+		sqltypes.NULL,
+		sqltypes.NewInt64(5),
+		sqltypes.NewInt64(6),
+		sqltypes.NewInt64(7),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	want := []key.Destination{
+		key.DestinationKeyspaceID([]byte("\x16k@\xb4J\xbaK\xd6")),
+		key.DestinationKeyspaceID([]byte("\x06\xe7\xea\"Βp\x8f")),
+		key.DestinationKeyspaceID([]byte("N\xb1\x90ɢ\xfa\x16\x9c")),
+		key.DestinationNone{},
+		key.DestinationKeyspaceID([]byte("\xd2\xfd\x88g\xd5\r-\xfe")),
+		key.DestinationKeyspaceID([]byte("p\xbb\x02<\x81\f\xa8z")),
+		key.DestinationKeyspaceID([]byte("\xf0\x98H\n\xc4ľq")),
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Map(): %#v, want %+v", got, want)
+	}
+}
+
+func TestHashOffsetVerify(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	ids := []sqltypes.Value{sqltypes.NewInt64(0), sqltypes.NewInt64(1)}
+	ksids := [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6"), []byte("\x16k@\xb4J\xbaK\xd6")}
+	got, err := hashOffset.Verify(nil, ids, ksids)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []bool{true, false}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("binaryMD5.Verify: %v, want %v", got, want)
+	}
+
+	// Failure test
+	_, err = hashOffset.Verify(nil, []sqltypes.Value{sqltypes.NewVarBinary("aa")}, [][]byte{nil})
+	wantErr := "hash.Verify: could not parse value: 'aa'"
+	if err == nil || err.Error() != wantErr {
+		t.Errorf("hashOffset.Verify err: %v, want %s", err, wantErr)
+	}
+}
+
+func TestHashOffsetReverseMap(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	got, err := hashOffset.(Reversible).ReverseMap(nil, [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6")})
+	if err != nil {
+		t.Error(err)
+	}
+	want := []sqltypes.Value{sqltypes.NewUint64(uint64(0))}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("ReverseMap(): %v, want %v", got, want)
+	}
+}
+
+func TestHashOffsetReverseMapNeg(t *testing.T) {
+	hashOffset := createHashOffset("1")
+
+	_, err := hashOffset.(Reversible).ReverseMap(nil, [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6\x16k@\xb4J\xbaK\xd6")})
+	want := "invalid keyspace id: 166b40b44aba4bd6166b40b44aba4bd6"
+	if err.Error() != want {
+		t.Error(err)
+	}
+}
