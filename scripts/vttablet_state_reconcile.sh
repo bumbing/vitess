3cbd05d7b984fc8e3dc16a7fc454c184609a1d01
huiqing <hzhou@pinterest.com>
2019-02-21 14:22:49 -0800

    adding a cron job to vtctld which reconciles state drift between vitess topo and configv3 meta
    Summary:
    the problem was as stated in: https://phabricator.pinadmin.com/D365877
    but instead of using puppet to enforce this on every vttablet host, we could do cron from `vtctld` which has more redundant resource for the job.
    
    1. gather all master tablets
    2. do `python -m vitess_utils.vitess -a vttabletstatechange --host $TABLET` on each
    
    Reviewers: #vitess, dweitzman, epruitt
    
    Reviewed By: #vitess, epruitt
    
    Subscribers: epruitt, jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D366272

---
+++ scripts/vttablet_state_reconcile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+
+# find all masters from all keyspaces
+# sample from: hzhou@vtctld-latest-0a03599a:~$ sudo docker exec -ti vtctld /vt/bin/vtctlclient -server localhost:15991 ListAllTablets | grep master
+# test-0000000202 patio -80 master coladevdb-2-2.ec2.pin220.com:15103 coladevdb-2-2.ec2.pin220.com:3306 []
+# test-0000000301 patio 80- master coladevdb-3-1.ec2.pin220.com:15103 coladevdb-3-1.ec2.pin220.com:3306 []
+# test-0000001024 patiogeneral 0 master devpatiogeneraldb-1-24.ec2.pin220.com:15103 devpatiogeneraldb-1-24.ec2.pin220.com:3306 []
+# test-0000001046 patio 0 master coladevdb-1-46.ec2.pin220.com:15103 coladevdb-1-46.ec2.pin220.com:3306 []
+MASTERS="$(/vt/bin/vtctlclient -server localhost:15991 ListAllTablets | grep master | cut -d ' ' -f 5 | cut -d '.' -f 1)"
+YESTERDAY=$(date --date=yesterday "+%Y-%m-%d")
+TODAY=$(date "+%Y-%m-%d")
+
+rm -f /vt/logs/sync-$YESTERDAY-*.log
+for TABLET in $MASTERS; do
+    # do a state sync between vitess topology and configv3 meta
+    python -m vitess_utils.vitess -a vttabletstatechange --host $TABLET >> /vt/logs/"sync-$TODAY-$TABLET.log"
+done
