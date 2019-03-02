3576173c96e6a6492438af415865461c842a46a1
David Weitzman <dweitzman@pinterest.com>
2019-03-02 08:39:23 -0800

    Simplify pvtctl.sh from darwin/laptop
    Summary: Pre-escape the arguments so they get through ok and invoke vtctlclient remotely instead of locally
    
    Reviewers: hzhou, #vitess, mingjianliu
    
    Reviewed By: #vitess, mingjianliu
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D370839

--- scripts/pvtctl.sh
+++ scripts/pvtctl.sh
@@ -11,21 +11,14 @@ fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Proxy through vtctld from a mac
-    # Use a different local port number for each host type to make sure
-    # we don't get confused an send a command intented to latest when
-    # the proxy is pointing to prod.
     if [[ "$ENV_NAME" == "prod" ]]; then
         HOST_TYPE="vtctld-prod"
-        TUNNEL_PORT=16127
     elif [[ "$ENV_NAME" == "shadow" ]]; then
         HOST_TYPE="vtctld-shadow"
-        TUNNEL_PORT=16128
     elif [[ "$ENV_NAME" == "latest" ]]; then
         HOST_TYPE="vtctld-latest"
-        TUNNEL_PORT=16129
     elif [[ "$ENV_NAME" == "test" ]]; then
         HOST_TYPE="vtctld-test"
-        TUNNEL_PORT=16130
     else
         echo "Looks like an invalid env name for a mac. Valid names tend to look like prod, latest, test, etc."
         echo "Usage: pvtcl.sh [vitess env] [args to vtctl...]"
@@ -37,26 +30,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         echo "fh lookup of hostname failed"
         exit 1
     fi
-    
-    # Start an ssh tunnel specific to this host, or reuse one that already exists.
-    # Each host type users a separate local port to make sure there's no risk of
-    # getting confused about which type of vtctl you're sending commands to.
-    if ! ssh -q -O check -S ~/.ssh/controlmaster-pvtctl-"$HOST_NAME" "$HOST_NAME" > /dev/null 2>&1; then
-       ssh -q -M -L $TUNNEL_PORT:localhost:15991 -f -o ExitOnForwardFailure=yes -S ~/.ssh/controlmaster-pvtctl-"$HOST_NAME" "$HOST_NAME" sleep 120 > /dev/null 2>&1
-    fi
 
-    # NOTE(dweitzman): This "go run" command will only work if your GOPATH
-    # is all set up or alternative if you create a go.mod file using
-    # ~/code/dw/scripts/vt_fix_gomod.sh
-    #
-    # Longer term, we should probably put a homebrew package at
-    # https://phabricator.pinadmin.com/diffusion/BREW/browse/master/
-    #
-    # We could actually even use an off-the-shelf vtctlclient here if upstream
-    # (open source) vitess published a homebrew package.
-    go run ./go/cmd/vtctlclient -server localhost:$TUNNEL_PORT "$@"
-    # Close the port forwarding.
-    ssh -q -O exit -S ~/.ssh/controlmaster-pvtctl-"$HOST_NAME" "$HOST_NAME"
+    # Escape all the args to make sure they get passed through ok through ssh to the docker command
+    escaped_args=()
+    for arg in "$@"; do
+        escaped_args=( "${escaped_args[@]:-}" "$(printf '%q' "$arg")" )
+    done
+
+    ssh -t "$HOST_NAME" sudo docker exec -ti vtctld /vt/bin/vtctlclient -action_timeout 5m -server localhost:15991 "${escaped_args[@]}"
     exit
 fi
 
