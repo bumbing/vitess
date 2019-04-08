e01dda62c563972a17ed1f5f45058999c5d9b7c0
huiqing <hzhou@pinterest.com>
2019-04-08 10:23:17 -0700

    alert vschema drift
    Summary:
    this is the discovery part of table schema => vschema semi sync process.
    it's set up as a `vtctld` cron job, which checks every 1 hour, using the `ddl` snapshot to generate a deterministic `vschema` version, and compare with the active one.
    whenever a diff is found, if not suppressed (in `dev`), we'll fire an alert to #vitess-alerts channel to call for attention.
    
    more details in: https://docs.google.com/document/d/1zAIsxjdZPLZTVa64C4IWgjlT1v2942yMfaWuiItzqDE/
    
    Test Plan: this is to first get deployed to vtctld-test-* & vtctld-latest-*, we could check the cron log and see if it's doing the right thing.
    
    Reviewers: #vitess, mingjianliu, dweitzman
    
    Reviewed By: #vitess, mingjianliu
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D384111

---
+++ scripts/alert_vschema_drift.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# the only keyspace currently sharded, and require monitoring is `patio`
+KEYSPACE="patio"
+# need one of the shards to get `ddl` from
+SAMPLE_SHARD="-80"
+# suppress alert
+SUPPRESS_ALERT=false
+
+if [[ $# -gt 0 && "$1" == "--suppress" ]]; then
+    echo "[drift] --suppress option enabled"
+    SUPPRESS_ALERT=true
+fi
+
+# `pinschema` arguments used to generate the `vschema`
+if [[ ${TELETRAAN_ENV_TYPE:-test} == "prod" ]]; then
+    # this applies to both `patio` in `prod` & `shadow`
+    KEYSPACE_ARGS=(
+        -include-cols -cols-authoritative -create-sequences
+        -create-primary-vindexes -create-secondary-vindexes
+        -default-scatter-cache-capacity 100000
+        -table-scatter-cache-capacity "campaigns:200000,product_groups:1000000"
+    )
+else
+    KEYSPACE_ARGS=(
+        -create-sequences -include-cols -cols-authoritative
+        -create-primary-vindexes -create-secondary-vindexes
+        -default-scatter-cache-capacity 100000
+    )
+fi
+
+VTCTL_CMD="/vt/bin/vtctlclient -server localhost:15991"
+PINSCHEMA_CMD="/vt/bin/pinschema"
+ALERT_CMD="python -m vitess_utils.alerts"
+
+if ! $VTCTL_CMD ValidateSchemaKeyspace "${KEYSPACE}"; then
+    echo "[drift] Bypassing vschema drift check, keyspace: ${KEYSPACE} under change"
+    exit 0
+fi
+
+echo "[drift] Finding tablets from ${KEYSPACE}/${SAMPLE_SHARD} to pull schemas from..."
+KEYSPACE_MASTER=$($VTCTL_CMD ListAllTablets | grep " ${KEYSPACE} ${SAMPLE_SHARD} " | grep " master " | cut -d' ' -f 1)
+
+echo "[drift] Snapshotting current ${KEYSPACE} schema..."
+KEYSPACE_SCHEMA_FILE=$(mktemp -t schema-"${KEYSPACE}".sql.XXXX)
+KEYSPACE_SCHEMA_CONTENT=$($VTCTL_CMD GetSchema "$KEYSPACE_MASTER" | jq -r '.table_definitions[].schema + ";"')
+echo "$KEYSPACE_SCHEMA_CONTENT" > "$KEYSPACE_SCHEMA_FILE"
+
+echo "[drift] Generating ${KEYSPACE} vschema ..."
+KEYSPACE_VSCHEMA=$($PINSCHEMA_CMD create-vschema "${KEYSPACE_ARGS[@]}" "$KEYSPACE_SCHEMA_FILE")
+KEYSPACE_VSCHEMA_FILE=$(mktemp -t vschema-"${KEYSPACE}".json.XXXX)
+echo "$KEYSPACE_VSCHEMA" > "$KEYSPACE_VSCHEMA_FILE"
+
+echo "[drift] Diffing ${KEYSPACE} vschema..."
+KEYSPACE_VSCHEMA_OLD=$($VTCTL_CMD GetVSchema ${KEYSPACE})
+DIFF=$(diff --strip-trailing-cr -u <(echo "$KEYSPACE_VSCHEMA_OLD") <(echo "$KEYSPACE_VSCHEMA") || test "$?" -eq 1)
+KEYSPACE_VSCHEMA_DIFF_FILE=$(mktemp -t vschema-"${KEYSPACE}".diff.XXXX)
+if [ "$DIFF" ]; then
+    echo "$DIFF" > "$KEYSPACE_VSCHEMA_DIFF_FILE"
+    echo "[drift] Reporting diff for ${KEYSPACE} with bot suppressed=${SUPPRESS_ALERT}"
+    if [[ "$SUPPRESS_ALERT" != true ]]; then
+        $ALERT_CMD --message "vschema drift detected in ${KEYSPACE}" --attach "$KEYSPACE_VSCHEMA_DIFF_FILE"
+    fi
+fi
+
+echo "[drift] Cleaning up temp files..."
+rm -f "$KEYSPACE_SCHEMA_FILE"
+rm -f "$KEYSPACE_VSCHEMA_FILE"
+rm -f "$KEYSPACE_VSCHEMA_DIFF_FILE"
