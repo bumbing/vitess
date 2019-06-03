aa65df00388cf716c293e906f81f7c90408d8db6
huiqing <hzhou@pinterest.com>
2019-06-03 15:52:04 -0700

    actually, we have only `pip module` of `vitess_utils` in the `vtctld` image, and we need to give the master hosts as params
    Summary: after testing in `vtctld-test-*`, found a couple issues that needs to be patched. mainly the fact that we have only `pip module` in `vtctld` image for `vitess_utils`.
    
    Reviewers: #vitess, chengchenghu, mingjianliu
    
    Reviewed By: #vitess, mingjianliu
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D407456

--- scripts/alert_shard_tablet_master_drift.sh
+++ scripts/alert_shard_tablet_master_drift.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if command -v /usr/local/bin/vitess_utils/vitess_utils.sh >/dev/null; then
-    /usr/local/bin/vitess_utils/vitess_utils.sh --action validateshardtabletmaster
-fi
+# check with each vitess `master`, see if topology is the same as mysql's
+MASTERS="$(/vt/bin/vtctlclient -server localhost:15991 ListAllTablets | grep master | cut -d ' ' -f 5 | cut -d '.' -f 1)"
+for TABLET in $MASTERS; do
+    python -m vitess_utils.vitess --action validateshardtabletmaster --host "$TABLET"
+done
