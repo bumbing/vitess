52538fcddfcaaaa912219f266b942b977b062fd5
David Weitzman <dweitzman@pinterest.com>
2019-08-07 09:55:12 -0700

    Testing emergency land. Please don't review
    Summary: #ThisIsAnEmergencyLetMeLand
    
    Test Plan: Testing for sox review
    
    Reviewers: chengchenghu, O842 Vitess SOX!
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D434493

--- scripts/fix_gomod.sh
+++ scripts/fix_gomod.sh
@@ -7,7 +7,7 @@ set -o pipefail
 # The upgrade of cloud.google.com/go is to avoid an incompatibility with the version of gax-go
 # that "go mod init" picks.
 
-# The test command compiles tests without running any
+# The test command compiles tests without running any.
 
 go mod init && \
     sed -i '' 's_cloud.google.com/go .*_cloud.google.com/go v0.36.0_' go.mod
