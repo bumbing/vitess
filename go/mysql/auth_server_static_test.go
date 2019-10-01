d9d7e649cc7749404dcf4722d2f74288e4a4dd8f
David Weitzman <dweitzman@pinterest.com>
2019-10-01 15:07:54 -0700

    Pull some potentially flaky tests into flaky_test.go files
    Summary:
    Ideally this change would happen upstream, but it might be that some difference between our environment
    and travisci makes them more flaky for us than for others.
    
    We can at least experiment to see if this helps with overall test reliability
    
    Test Plan:
    ```
    docker build --no-cache -f docker/base/Dockerfile.percona --build-arg BASE_IMAGE=998131032990.dkr.ecr.us-east-1.amazonaws.com/vitess/bootstrap:percona -t 998131032990.dkr.ecr.us-east-1.amazonaws.com/vitess/base:foo . && docker build --no-cache -f Dockerfile.pinterest --build-arg BASE_IMAGE=998131032990.dkr.ecr.us-east-1.amazonaws.com/vitess/base:foo .
    ```
    
    Reviewers: hzhou, O842 Vitess SOX, mingjianliu
    
    Reviewed By: O842 Vitess SOX, mingjianliu
    
    Subscribers: jenkins, mleonard
    
    Differential Revision: https://phabricator.pinadmin.com/D459417

--- go/mysql/auth_server_static_test.go
+++ go/mysql/auth_server_static_test.go
@@ -135,6 +135,13 @@ func TestStaticConfigHUP(t *testing.T) {
 	}
 
 	InitAuthServerStatic()
+	defer func() {
+		// delete registered Auth server
+		for auth := range authServers {
+			delete(authServers, auth)
+		}
+	}()
+
 	aStatic := GetAuthServer("static").(*AuthServerStatic)
 
 	if aStatic.Entries[oldStr][0].Password != oldStr {
@@ -143,40 +150,6 @@ func TestStaticConfigHUP(t *testing.T) {
 
 	hupTest(t, tmpFile, oldStr, "str2")
 	hupTest(t, tmpFile, "str2", "str3") // still handling the signal
-
-	// delete registered Auth server
-	for auth := range authServers {
-		delete(authServers, auth)
-	}
-}
-
-func TestStaticConfigHUPWithRotation(t *testing.T) {
-	tmpFile, err := ioutil.TempFile("", "mysql_auth_server_static_file.json")
-	if err != nil {
-		t.Fatalf("couldn't create temp file: %v", err)
-	}
-	defer os.Remove(tmpFile.Name())
-	*mysqlAuthServerStaticFile = tmpFile.Name()
-
-	savedReloadInterval := *mysqlAuthServerStaticReloadInterval
-	defer func() { *mysqlAuthServerStaticReloadInterval = savedReloadInterval }()
-	*mysqlAuthServerStaticReloadInterval = 10 * time.Millisecond
-
-	oldStr := "str1"
-	jsonConfig := fmt.Sprintf("{\"%s\":[{\"Password\":\"%s\"}]}", oldStr, oldStr)
-	if err := ioutil.WriteFile(tmpFile.Name(), []byte(jsonConfig), 0600); err != nil {
-		t.Fatalf("couldn't write temp file: %v", err)
-	}
-
-	InitAuthServerStatic()
-	aStatic := GetAuthServer("static").(*AuthServerStatic)
-
-	if aStatic.Entries[oldStr][0].Password != oldStr {
-		t.Fatalf("%s's Password should still be '%s'", oldStr, oldStr)
-	}
-
-	hupTestWithRotation(t, tmpFile, oldStr, "str4")
-	hupTestWithRotation(t, tmpFile, "str4", "str5")
 }
 
 func hupTest(t *testing.T, tmpFile *os.File, oldStr, newStr string) {
---
+++ go/vt/vttablet/tabletserver/vstreamer/vstreamer_flaky_test.go
@@ -0,0 +1,102 @@
+package vstreamer
+
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
+)
+
+func TestREKeyRange(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	execStatements(t, []string{
+		"create table t1(id1 int, id2 int, val varbinary(128), primary key(id1))",
+	})
+	defer execStatements(t, []string{
+		"drop table t1",
+	})
+	engine.se.Reload(context.Background())
+
+	if err := env.SetVSchema(shardedVSchema); err != nil {
+		t.Fatal(err)
+	}
+	defer env.SetVSchema("{}")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	filter := &binlogdatapb.Filter{
+		Rules: []*binlogdatapb.Rule{{
+			Match:  "/.*/",
+			Filter: "-80",
+		}},
+	}
+	ch := startStream(ctx, t, filter)
+
+	// 1, 2, 3 and 5 are in shard -80.
+	// 4 and 6 are in shard 80-.
+	input := []string{
+		"begin",
+		"insert into t1 values (1, 4, 'aaa')",
+		"insert into t1 values (4, 1, 'bbb')",
+		// Stay in shard.
+		"update t1 set id1 = 2 where id1 = 1",
+		// Move from -80 to 80-.
+		"update t1 set id1 = 6 where id1 = 2",
+		// Move from 80- to -80.
+		"update t1 set id1 = 3 where id1 = 4",
+		"commit",
+	}
+	execStatements(t, input)
+	expectLog(ctx, t, input, ch, [][]string{{
+		`gtid|begin`,
+		`gtid|begin`,
+		`type:FIELD field_event:<table_name:"t1" fields:<name:"id1" type:INT32 > fields:<name:"id2" type:INT32 > fields:<name:"val" type:VARBINARY > > `,
+		`type:ROW row_event:<table_name:"t1" row_changes:<after:<lengths:1 lengths:1 lengths:3 values:"14aaa" > > > `,
+		`type:ROW row_event:<table_name:"t1" row_changes:<before:<lengths:1 lengths:1 lengths:3 values:"14aaa" > after:<lengths:1 lengths:1 lengths:3 values:"24aaa" > > > `,
+		`type:ROW row_event:<table_name:"t1" row_changes:<before:<lengths:1 lengths:1 lengths:3 values:"24aaa" > > > `,
+		`type:ROW row_event:<table_name:"t1" row_changes:<after:<lengths:1 lengths:1 lengths:3 values:"31bbb" > > > `,
+		`commit`,
+	}})
+
+	// Switch the vschema to make id2 the primary vindex.
+	altVSchema := `{
+  "sharded": true,
+  "vindexes": {
+    "hash": {
+      "type": "hash"
+    }
+  },
+  "tables": {
+    "t1": {
+      "column_vindexes": [
+        {
+          "column": "id2",
+          "name": "hash"
+        }
+      ]
+    }
+  }
+}`
+	if err := env.SetVSchema(altVSchema); err != nil {
+		t.Fatal(err)
+	}
+
+	// Only the first insert should be sent.
+	input = []string{
+		"begin",
+		"insert into t1 values (4, 1, 'aaa')",
+		"insert into t1 values (1, 4, 'aaa')",
+		"commit",
+	}
+	execStatements(t, input)
+	expectLog(ctx, t, input, ch, [][]string{{
+		`gtid|begin`,
+		`gtid|begin`,
+		`type:ROW row_event:<table_name:"t1" row_changes:<after:<lengths:1 lengths:1 lengths:3 values:"41aaa" > > > `,
+		`commit`,
+	}})
+}
